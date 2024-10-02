67b8f39c9b0d8224052ae4e55c2f6ec52a1fb923
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-10-01 21:32:51 -0300

    Initial Commit

---
+++ FotoroidTests/FotoroidTests.swift
@@ -0,0 +1,17 @@
+//
+//  FotoroidTests.swift
+//  FotoroidTests
+//
+//  Created by Eduardo Frederico on 01/10/24.
+//
+
+import Testing
+@testable import Fotoroid
+
+struct FotoroidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
