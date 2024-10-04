7a2be0e6aefe3a7bda0670d6ab41ef1cfeb2969c
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-10-03 21:42:10 -0300

    Tirando fotos com UIImagePickerController.

--- Fotoroid/EffectsViewController.swift
+++ Fotoroid/EffectsViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class EffectsViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var viLoading: UIView!
+    
+    
+    @IBOutlet weak var ivPhoto: UIImageView!
+    
+    var image: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ivPhoto.image = image
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-    */
 
 }
--- Fotoroid/ViewController.swift
+++ Fotoroid/ViewController.swift
@@ -13,7 +13,75 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! EffectsViewController
+        vc.image = sender as! UIImage
+    }
 
 
+    @IBAction func selectSource(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar foto", message: "De onde você quer escolher a foto", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default, handler: { (action) in
+                self.selectPicture(sourceType: .camera)
+            })
+            alert.addAction(cameraAction)
+        }
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default) { (action) in
+            self.selectPicture(sourceType: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        
+        let photoAction = UIAlertAction(title: "Álbum de Fotos", style: .default) { (action) in
+            self.selectPicture(sourceType: .savedPhotosAlbum)
+        }
+        alert.addAction(photosAction)
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func selectPicture(sourceType: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let originalImage = image.size.width
+            let aspectRatio = originalImage / image.size.height
+            var smallImage: CGSize
+            if aspectRatio > 1 {
+                smallImage = CGSize(width: 1000, height: 1000/aspectRatio)
+            } else {
+                smallImage = CGSize(width: 1000*aspectRatio, height: 1000)
+            }
+            
+            UIGraphicsBeginImageContext(smallSize)
+            image.draw(in: CGRect(x: 0, y: 0, width: smallSize.width, height: smallSize.height))
+            let smallImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            dismiss(animated: true, completion: {
+                self.performSegue(withIdentifier: "effectsSegue", sender: smallImage)
+            })
+            
+        }
+    }
+}
